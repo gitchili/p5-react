919abc35b6adf9d67bfacb248a578e5c742bb81d
gitchili <jameslphillips@gmail.com>
2018-11-21 11:48:15 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Sketch } from './components/sketch';
+// import { Sketch } from './components/canvas';
+
+// There are a few between a regular sketch for instance mode
+// First, it takes a width, height, and props, and returns
+// the _actual_ sketch that takes a p5 object (closing over
+// width, height and props, passed by the React component)
+// Second, do not use createCanvas! This is handled by the
+// Sketch component. To set the width/height of a sketch,
+// either apply CSS on the Sketch or pass it the appropriate
+// width/height props.
+// Finally, there are two optionally added functions:
+// - receiveProps, which can receive props
+// - unmount, triggered before a Sketch unmounts
+const sketch = (width, height, props) => {
+  return function(p5) {
+    let value = props.value;
+    p5.setup = () => {
+      p5.strokeWeight(50);
+    };
+
+    p5.draw = () => {
+      p5.fill(value, 16);
+      p5.noStroke();
+      p5.rect(0, 0, width, height);
+      p5.stroke((value + 128) % 256);
+      // p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+      p5.rect(50, 50, 50, 50);
+    };
+
+    p5.receiveProps = nextProps => {
+      console.log(nextProps.value);
+      value = nextProps.value;
+    };
+
+    p5.unmount = () => {
+      console.log(
+        'The sketch was unmounted. Width was ' +
+          width +
+          ', height was ' +
+          height
+      );
+    };
+  };
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+  }
+  render() {
+    return (
+      <>
+        <div
+          style={{ width: '100%', height: '100%' }}
+          // onClick={() => {
+          //   this.setState({ value: (this.state.value + 5) % 256 });
+          // }}
+        >
+          <Sketch
+            sketch={sketch}
+            width={'100%'}
+            height={'100%'}
+            sketchProps={{ value: this.state.value }}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
